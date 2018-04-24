e32d31d5cfdc61bb33ebef892415033c1906d64c
Akshata Mohanty <akshatamohanty@gmail.com>
2018-04-24 14:56:27 +0800

    updated ui for gallery and landing

---
+++ src/app/constants.ts
@@ -0,0 +1,5 @@
+export class Constants {
+  public static get GALLERY_URL(): string 
+  	{ return "https://api.github.com/repos/phtj/mobius-cesium/contents/src/assets/json-files?ref=master"; 
+  };
+}
--- src/app/ui-components/main/mobius-gallery/mobius-gallery.component.ts
+++ src/app/ui-components/main/mobius-gallery/mobius-gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
+import { Constants } from "../../../constants";
 
 @Component({
   selector: 'mobius-gallery',
@@ -9,17 +10,13 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class MobiusGalleryComponent implements OnInit {
 
-  static readonly gallery_url = 'https://api.github.com/repos/phtj/mobius-cesium/contents/src/assets/json-files?ref=master';
-
   private all_files;
-  //https://api.github.com/repos/phtj/mobius-cesium/contents/src/assets/json-files?ref=workshop-features
 
   constructor(private http: HttpClient) { 
-    this.http.get(MobiusGalleryComponent.gallery_url).subscribe(data => this.all_files = data);
+    this.http.get(Constants.GALLERY_URL).subscribe(data => this.all_files = data);
   }
 
   ngOnInit() {
-    console.log(this.all_files);
   }
 
 }
