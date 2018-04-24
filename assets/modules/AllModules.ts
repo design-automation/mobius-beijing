180ed7519de5da8c1a0cf6e8b5f95646e6fa6f4e
Akshata Mohanty <akshatamohanty@gmail.com>
2018-04-24 17:20:02 +0800

    Auto-generated commit

--- assets/modules/AllModules.ts
+++ assets/modules/AllModules.ts
@@ -1,19 +1,19 @@
 import {ModuleUtils, IModule} from "../../app/base-classes/code/CodeModule";
 
 //import * as GSS from "gs-modelling";
-// import docs from "turf-modelling/docs_json/turf-modelling.json";
+import docs from "turf-modelling/docs_json/turf-modelling.json";
 
-// import * as Turf from "turf-modelling";
-// let Math: IModule = ModuleUtils.createModule("Math", Turf["math"], "attrib", docs);
-// let Feature_Coll: IModule = ModuleUtils.createModule("Feature_Coll", Turf["feature_coll"], "attrib", docs);
-// let Properties: IModule = ModuleUtils.createModule("Properties", Turf["properties"], "attrib", docs);
 // let Measure: IModule = ModuleUtils.createModule("Measure", Turf["measure"], "attrib", docs);
 // 
 
 
-// export {Math, Feature_Coll, Properties, Measure};
 
+import * as TurfModelling from "turf-modelling";
 import * as trf from "turf";
+let Feature_Coll: IModule = ModuleUtils.createModule("Feature_Coll", TurfModelling["feature_coll"], "attrib", docs);
+let Math: IModule = ModuleUtils.createModule("Math", TurfModelling["math"], "attrib", docs);
+let Properties: IModule = ModuleUtils.createModule("Properties", TurfModelling["properties"], "attrib", docs);
 let Turf: IModule = ModuleUtils.createModule("Turf", trf, "attrib", undefined);
 
-export {Turf};
+// export {Turf};
+export {Math, Feature_Coll, Properties, Turf};
