6343cc62aba9da05da7a56653d42c60b6362f5a5
Akshata Mohanty <akshatamohanty@gmail.com>
2018-04-27 13:19:54 +0800

    Auto-generated commit

--- assets/modules/AllModules.ts
+++ assets/modules/AllModules.ts
@@ -10,42 +10,49 @@ import * as papaparse from "papaparse";
 import * as shapefile from "shpjs";
 //import * as math from "mathjs";
 
-var mathjs = require('mathjs');
+import * as mobiusgeojson from "mobius-geojson"; 
+
+let mathjs = require('mathjs');
 //var tj = require('togeojson');
 
 //let Feature_Coll: IModule = ModuleUtils.createModule("Feature_Coll", TurfModelling["feature_coll"], "attrib", docs);
-let Math: IModule = ModuleUtils.createModule("Math", mathjs/*TurfModelling["math"]*/, "attrib", undefined);
+let UT_Math: IModule = ModuleUtils.createModule("UT_Math", mathjs/*TurfModelling["math"]*/, "attrib", undefined);
 //let Togeojson: IModule = ModuleUtils.createModule("Togeojson", tj/*TurfModelling["math"]*/, "attrib", docs);
 //let Properties: IModule = ModuleUtils.createModule("Properties", TurfModelling["properties"], "attrib", docs);
 
-let Papaparse: IModule = ModuleUtils.createModule("Papaparse", papaparse, "attrib", undefined);
-let Shapefile: IModule = ModuleUtils.createModule("Shapefile", shapefile, "attrib", undefined);
+let FL_Papaparse: IModule = ModuleUtils.createModule("FL_Papaparse", papaparse, "attrib", undefined);
+let FL_Shapefile: IModule = ModuleUtils.createModule("FL_Shapefile", shapefile, "attrib", undefined);
 
 
-let List: IModule = ModuleUtils.createModule("List", GSS["list"], "attrib", docs);
+let UT_List: IModule = ModuleUtils.createModule("UT_List", GSS["list"], "attrib", docs);
 
-let Aggregate: IModule = ModuleUtils.createModule("Aggregate", TurfModelling["aggregate"], "attrib", docs);
-let Assert: IModule = ModuleUtils.createModule("Assert", TurfModelling["assert"], "attrib", docs);
-let Bool: IModule = ModuleUtils.createModule("Bool", TurfModelling["bool"], "attrib", docs);
-let Classify: IModule = ModuleUtils.createModule("Classify", TurfModelling["classify"], "attrib", docs);
-let Convert: IModule = ModuleUtils.createModule("Convert", TurfModelling["convert"], "attrib", docs);
-let Coords: IModule = ModuleUtils.createModule("Coords", TurfModelling["coords"], "attrib", docs);
-let Create: IModule = ModuleUtils.createModule("Create", TurfModelling["create"], "attrib", docs);
-let Data: IModule = ModuleUtils.createModule("Data", TurfModelling["data"], "attrib", docs);
-let Grids: IModule = ModuleUtils.createModule("Grids", TurfModelling["grids"], "attrib", docs);
-let Ipolate: IModule = ModuleUtils.createModule("Ipolate", TurfModelling["ipolate"], "attrib", docs);
-let Joins: IModule = ModuleUtils.createModule("Joins", TurfModelling["joins"], "attrib", docs);
-let Measure: IModule = ModuleUtils.createModule("Measure", TurfModelling["measure"], "attrib", docs);
-let Meta: IModule = ModuleUtils.createModule("Meta", TurfModelling["meta"], "attrib", docs);
-let Misc: IModule = ModuleUtils.createModule("Misc", TurfModelling["misc"], "attrib", docs);
-let Random: IModule = ModuleUtils.createModule("Random", TurfModelling["random"], "attrib", docs);
-let Units: IModule = ModuleUtils.createModule("Units", TurfModelling["units"], "attrib", docs);
-let Xform: IModule = ModuleUtils.createModule("Xform", TurfModelling["xform"], "attrib", docs);
+let TF_Aggregate: IModule = ModuleUtils.createModule("TF_Aggregate", TurfModelling["aggregate"], "attrib", undefined);
+let TF_Assert: IModule = ModuleUtils.createModule("TF_Assert", TurfModelling["assert"], "attrib", undefined);
+let TF_Bool: IModule = ModuleUtils.createModule("TF_Bool", TurfModelling["bool"], "attrib", undefined);
+let TF_Classify: IModule = ModuleUtils.createModule("TF_Classify", TurfModelling["classify"], "attrib", undefined);
+let TF_Convert: IModule = ModuleUtils.createModule("TF_Convert", TurfModelling["convert"], "attrib", undefined);
+let TF_Coords: IModule = ModuleUtils.createModule("TF_Coords", TurfModelling["coords"], "attrib", undefined);
+let TF_Create: IModule = ModuleUtils.createModule("TF_Create", TurfModelling["create"], "attrib", undefined);
+let TF_Data: IModule = ModuleUtils.createModule("TF_Data", TurfModelling["data"], "attrib", undefined);
+let TF_Grids: IModule = ModuleUtils.createModule("TF_Grids", TurfModelling["grids"], "attrib", undefined);
+let TF_Ipolate: IModule = ModuleUtils.createModule("TF_Ipolate", TurfModelling["ipolate"], "attrib", undefined);
+let TF_Joins: IModule = ModuleUtils.createModule("TF_Joins", TurfModelling["joins"], "attrib", undefined);
+let TF_Measure: IModule = ModuleUtils.createModule("TF_Measure", TurfModelling["measure"], "attrib", undefined);
+let TF_Meta: IModule = ModuleUtils.createModule("TF_Meta", TurfModelling["meta"], "attrib", undefined);
+let TF_Misc: IModule = ModuleUtils.createModule("TF_Misc", TurfModelling["misc"], "attrib", undefined);
+let TF_Random: IModule = ModuleUtils.createModule("TF_Random", TurfModelling["random"], "attrib", undefined);
+let TF_Units: IModule = ModuleUtils.createModule("TF_Units", TurfModelling["units"], "attrib", undefined);
+let TF_Xform: IModule = ModuleUtils.createModule("TF_Xform", TurfModelling["xform"], "attrib", undefined);
+
+
+let MJ_Collection: IModule = ModuleUtils.createModule("MJ_Collection", mobiusgeojson["collection"], "attrib", undefined);
+let MJ_Feature: IModule = ModuleUtils.createModule("MJ_Feature", mobiusgeojson["feature"], "attrib", undefined); 
 
 // export {Turf};
-export {Math,  
-		Papaparse, 
-		Shapefile, 
-	    List, 
-		Aggregate, Assert, Bool, Classify, Convert, Coords, Create, Data, Grids, Ipolate, Joins, Measure, Meta, Misc, Random, Units, Xform};
+export {UT_Math,  
+		FL_Papaparse, 
+		FL_Shapefile, 
+	    UT_List, 
+	    MJ_Collection, MJ_Feature,
+		TF_Aggregate, TF_Assert, TF_Bool, TF_Classify, TF_Convert, TF_Coords, TF_Create, TF_Data, TF_Grids, TF_Ipolate, TF_Joins, TF_Measure, TF_Meta, TF_Misc, TF_Random, TF_Units, TF_Xform};
 
