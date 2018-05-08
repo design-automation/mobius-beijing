113fc662b653f82c1d4c54fe64fc327a33b1944e
Akshata Mohanty <akshatamohanty@gmail.com>
2018-05-08 15:15:54 +0800

    loading gif

--- src/app/ui-components/viewers/viewer-container/viewer-container.component.ts
+++ src/app/ui-components/viewers/viewer-container/viewer-container.component.ts
@@ -69,8 +69,6 @@ export class ViewerContainerComponent extends Viewer implements OnInit {
 
 		let portType = parseInt(selectedPort.getType());
 
-		console.log(portType);
-
 		// todo: refactor 
 		switch(portType){
 			case 0: 
