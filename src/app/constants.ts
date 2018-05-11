656b0fa8a53744d0918740f80fa17453cd0afb97
Akshata Mohanty <akshatamohanty@gmail.com>
2018-05-11 22:40:25 +0800

    file path for gallery updated

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,9 +1,9 @@
 export class Constants {
 	public static get GALLERY_URL(): string 
-		{ return "https://api.github.com/repos/design-automation/mobius-cesium/contents/src/assets/json-files?ref=master"; 
+		{ return "https://api.github.com/repos/design-automation/mobius-gallery/contents/examples?ref=master"; 
 	};
 
 	public static get FILE_URL(): string { 
-			return "https://raw.githubusercontent.com/design-automation/mobius-cesium/master/src/assets/json-files/"
+			return "https://raw.githubusercontent.com/design-automation/mobius-gallery/master/examples/";
 	};
 }
